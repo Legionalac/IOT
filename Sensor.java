9c185a29e53b8af2d5fad826dfa67828123788f2
Nikola Pavicevic <nikola@pop-os.localdomain>
2023-06-05 11:27:59 +0200

    Paho library

--- Sensor.java
+++ Sensor.java
@@ -76,7 +76,6 @@ public class Sensor
                         System.out.println("Sending message...");
                     }
                 }
-                
             }
             catch(SocketException se)
             {
